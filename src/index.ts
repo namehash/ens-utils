bc32a2eda308fbcf7a989f9f0be7ac0bba9f592d
frankind.eth <faguzzi3@gmail.com>
2024-03-26 09:37:58 -0300

    feat: include nameparser in ens-utils and remove monorepo architecture

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,6 @@ export * from "./currency";
 export * from "./number";
 export * from "./price";
 export * from "./time";
+export * from "./ensname";
+export * from "./hashutils";
+export * from "./nameparser";
