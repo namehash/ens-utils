8211acea90f8ceaf7fbe08a0530aaa3a44563c66
lightwalker.eth <126201998+lightwalker-eth@users.noreply.github.com>
2024-04-26 12:54:44 +0400

    feat: enhance buildNFTRefFromENSName

--- src/ethregistrar.test.ts
+++ src/ethregistrar.test.ts
@@ -1,44 +1,57 @@
 import { describe, it, expect } from "vitest";
-import { UNWRAPPED_MAINNET_ETH_REGISTRAR, WRAPPED_MAINNET_ETH_REGISTRAR, buildNFTRefFromENSName } from "./ethregistrar";
-import { buildENSName } from "./ensname";
+import { Registrar, UNWRAPPED_MAINNET_ETH_REGISTRAR, WRAPPED_MAINNET_ETH_REGISTRAR, buildNFTRefFromENSName } from "./ethregistrar";
+import { ENSName, buildENSName } from "./ensname";
 import { MAINNET, SEPOLIA } from "./chain";
-import { buildNFTRef, buildTokenId } from "./nft";
+import { buildNFTRef } from "./nft";
 
 // TODO: add a lot more unit tests here
+
+function testNFTRefFromRegistrar(name: ENSName, registrar: Registrar, isWrapped: boolean): void {
+    const expectedToken = registrar.getTokenId(name, isWrapped);
+    const expectedNFT = buildNFTRef(registrar.contract, expectedToken);
+    const result = buildNFTRefFromENSName(name, registrar.contract.chain, isWrapped);
+    expect(result).toStrictEqual(expectedNFT);
+}
 
 describe("buildNFTRefFromENSName", () => {
 
     it("unrecognized registrar", () => {
-        const result = buildNFTRefFromENSName(buildENSName("foo.eth"), SEPOLIA, false);
-        expect(result).toBe(null);
+        expect(() => buildNFTRefFromENSName(buildENSName("foo.eth"), SEPOLIA, false)).toThrow();
     });
 
-    it("non-.eth TLD", () => {
-        const result = buildNFTRefFromENSName(buildENSName("foo.com"), MAINNET, false);
-        expect(result).toBe(null);
+    it("unwrapped non-.eth TLD", () => {
+        expect(() => buildNFTRefFromENSName(buildENSName("foo.com"), MAINNET, false)).toThrow();
     });
 
-    it("subname of a .eth subname", () => {
-        const result = buildNFTRefFromENSName(buildENSName("x.foo.eth"), MAINNET, false);
-        expect(result).toBe(null);
+    it("wrapped non-.eth TLD", () => {
+        const name = buildENSName("foo.com");
+        const registrar = WRAPPED_MAINNET_ETH_REGISTRAR;
+        const isWrapped = true;
+        testNFTRefFromRegistrar(name, registrar, isWrapped);
     });
 
-    it("non-normalized name", () => {
-        const result = buildNFTRefFromENSName(buildENSName("FOO.eth"), MAINNET, false);
-        expect(result).toBe(null);
+    it("unwrapped subname of a .eth subname", () => {
+        expect(() => buildNFTRefFromENSName(buildENSName("x.foo.eth"), MAINNET, false)).toThrow();
+    });
+
+    it("wrapped subname of a .eth subname", () => {
+        const name = buildENSName("x.foo.eth");
+        const registrar = WRAPPED_MAINNET_ETH_REGISTRAR;
+        const isWrapped = true;
+        testNFTRefFromRegistrar(name, registrar, isWrapped);
     });
 
     it("unwrapped direct subname of .eth", () => {
-        const token = buildTokenId(29714174079724412745887019504253973571029824035614949642323418802670541573197n);
-
-        const result = buildNFTRefFromENSName(buildENSName("foo.eth"), MAINNET, false);
-        expect(result).toStrictEqual(buildNFTRef(UNWRAPPED_MAINNET_ETH_REGISTRAR, token));
+        const name = buildENSName("foo.eth");
+        const registrar = UNWRAPPED_MAINNET_ETH_REGISTRAR;
+        const isWrapped = false;
+        testNFTRefFromRegistrar(name, registrar, isWrapped);
     });
 
     it("wrapped direct subname of .eth", () => {
-        const token = buildTokenId(100687382630207452182552124992380359340282681784978135003332851042988456540239n);
-
-        const result = buildNFTRefFromENSName(buildENSName("foo.eth"), MAINNET, true);
-        expect(result).toStrictEqual(buildNFTRef(WRAPPED_MAINNET_ETH_REGISTRAR, token));
+        const name = buildENSName("foo.eth");
+        const registrar = WRAPPED_MAINNET_ETH_REGISTRAR;
+        const isWrapped = true;
+        testNFTRefFromRegistrar(name, registrar, isWrapped);
     });
 });
--- src/ethregistrar.ts
+++ src/ethregistrar.ts
@@ -1,67 +1,118 @@
-import { ENSName, ETH_TLD } from "./ensname";
+import { ENSName, MIN_ETH_REGISTRABLE_LABEL_LENGTH, ETH_TLD, charCount } from "./ensname";
 import { NFTRef, TokenId, buildNFTRef, buildTokenId } from "./nft";
 import { namehash, labelhash } from 'viem/ens'
-import { buildAddress, isAddressEqual } from "./address";
+import { buildAddress } from "./address";
 import { ChainId, MAINNET } from "./chain";
-import { assert } from "vitest";
 import { ContractRef, buildContractRef } from "./contract";
 
+export interface Registrar {
+  contract: ContractRef;
+
+  getTokenId(name: ENSName, isWrapped: boolean): TokenId;
+  isClaimable(name: ENSName, isWrapped: boolean): boolean;
+}
+
 // known registrars
-export const WRAPPED_MAINNET_ETH_REGISTRAR =
+export const WRAPPED_MAINNET_ETH_REGISTRAR_CONTRACT =
   buildContractRef(MAINNET, buildAddress("0xD4416b13d2b3a9aBae7AcD5D6C2BbDBE25686401"));
-export const UNWRAPPED_MAINNET_ETH_REGISTRAR =
+export const UNWRAPPED_MAINNET_ETH_REGISTRAR_CONTRACT =
   buildContractRef(MAINNET, buildAddress("0x57f1887a8BF19b14fC0dF6Fd9B2acc9Af147eA85"));
 
-/**
- * Identifies if the provided name is a direct subname of ".eth".
- * 
- * @param name the name to evaluate.
- * @returns true if and only if the name is a direct subname of ".eth".
- */
-export function belongsToEthRegistar(name: ENSName): boolean {
-    
-    // all direct subnames of ".eth" have exactly 2 labels
+
+export class NameWrapper implements Registrar {
+
+  constructor(public contract: ContractRef) {
+    this.contract = contract;
+  }
+
+  getTokenId(name: ENSName, isWrapped: boolean): TokenId {
+    if (!this.isClaimable(name, isWrapped)) {
+      throw new Error(`Wrapped tokenId for name: "${name.name}" is not claimable by registrar: ${this.contract.address} on chainId: ${this.contract.chain.chainId}`);
+    }
+    return buildTokenId(BigInt(namehash(name.name)));
+  }
+
+  isClaimable(name: ENSName, isWrapped: boolean): boolean {
+    if (!isWrapped) return false;
+
+    if (name.labels.length >= 2) {
+      if (name.labels[1] === ETH_TLD) {
+        // first label must be of sufficient length
+        if (charCount(name.labels[0]) < MIN_ETH_REGISTRABLE_LABEL_LENGTH) return false;
+      }
+    }
+
+    // TODO: refine this. For example, there's a maximum length limit, etc.
+    return true;
+  }
+};
+
+export class ClassicETHRegistrarController implements Registrar {
+
+  constructor(public contract: ContractRef) {
+    this.contract = contract;
+  }
+
+  getTokenId(name: ENSName, isWrapped: boolean): TokenId {
+    if (!this.isClaimable(name, isWrapped)) {
+      throw new Error(`Unwrapped tokenId for name: "${name.name}" is not claimable by registrar: ${this.contract.address} on chainId: ${this.contract.chain.chainId}`);
+    }
+    return buildTokenId(BigInt(labelhash(name.labels[0])));
+  }
+
+  isClaimable(name: ENSName, isWrapped: boolean): boolean {
+    // name must be unwrapped
+    if (isWrapped) return false;
+
+    // must have exactly 2 labels to be a direct subname of ".eth"
     if (name.labels.length !== 2) return false;
 
     // last label must be "eth"
-    return name.labels[1] === ETH_TLD;
+    if (name.labels[1] !== ETH_TLD) return false;
+
+    // first label must be of sufficient length
+    return charCount(name.labels[0]) >= MIN_ETH_REGISTRABLE_LABEL_LENGTH;
+  }
+};
+
+export const WRAPPED_MAINNET_ETH_REGISTRAR = new NameWrapper(WRAPPED_MAINNET_ETH_REGISTRAR_CONTRACT);
+export const UNWRAPPED_MAINNET_ETH_REGISTRAR = new ClassicETHRegistrarController(UNWRAPPED_MAINNET_ETH_REGISTRAR_CONTRACT);
+
+export const KNOWN_REGISTRARS = [WRAPPED_MAINNET_ETH_REGISTRAR, UNWRAPPED_MAINNET_ETH_REGISTRAR];
+
+export function getPotentialKnownRegistrars(name: ENSName, chain: ChainId, isWrapped: boolean): Registrar[] {
+    return KNOWN_REGISTRARS.filter(registrar => registrar.contract.chain.chainId === chain.chainId &&
+                                                registrar.isClaimable(name, isWrapped));
 }
 
 /**
- * Identifies the registrar smart contract for the provided name, if known.
+ * Identifies the registrar for the provided name, if known.
  * 
  * @param name the name to evaluate.
  * @param chainId the id of the chain the name is managed on.
  * @param isWrapped if the name is wrapped or not.
- * @returns the contract of the registrar for the provided name and chainId or `null` if the registrar is not known.
+ * @returns the requested registrar
  */
-export function getKnownRegistrar(name: ENSName, chain: ChainId, isWrapped: boolean): ContractRef | null {
-    if (!belongsToEthRegistar(name)) return null;
-
-    // TODO: Add confirmed handling for additional chains.
-    if (chain.chainId !== MAINNET.chainId) return null;
-
-    return isWrapped ? WRAPPED_MAINNET_ETH_REGISTRAR : UNWRAPPED_MAINNET_ETH_REGISTRAR;
+export function getKnownRegistrar(name: ENSName, chain: ChainId, isWrapped: boolean): Registrar {
+    const registrars = getPotentialKnownRegistrars(name, chain, isWrapped);
+    if (registrars.length > 1) {
+        throw new Error(`Multiple potential registrars found for name: "${name.name}" on chainId: ${chain.chainId} when isWrapped: ${isWrapped}`);
+    } else if (registrars.length === 0) {
+        throw new Error(`No known registrars found for name: "${name.name}" on chainId: ${chain.chainId} when isWrapped: ${isWrapped}`);
+    } else {
+        return registrars[0];
+    }
 }
 
-export function buildNFTRefFromENSName(name: ENSName, chain: ChainId, isWrapped: boolean): NFTRef | null {
-    // TODO: better handle the case of unnormalized or unknown names
-    if (name.normalization !== "normalized") return null;
+export function getKnownPotentialNFTRefs (name: ENSName, chain: ChainId): NFTRef[] {
+  const wrappedNFT = buildNFTRefFromENSName(name, chain, true);
+  const unwrappedNFT = buildNFTRefFromENSName(name, chain, false);
+  return [wrappedNFT, unwrappedNFT].filter(nft => nft !== null);
+};
 
+export function buildNFTRefFromENSName(name: ENSName, chain: ChainId, isWrapped: boolean): NFTRef {
     const registrar = getKnownRegistrar(name, chain, isWrapped);
+    const token = registrar.getTokenId(name, isWrapped);
 
-    if (registrar === null) return null;
-
-    let token : TokenId;
-
-    if (isAddressEqual(registrar.address, WRAPPED_MAINNET_ETH_REGISTRAR.address)) {
-      token = buildTokenId(BigInt(namehash(name.name)));
-    } else if (isAddressEqual(registrar.address, UNWRAPPED_MAINNET_ETH_REGISTRAR.address)) {
-      assert(name.labels.length === 2, `Name: "${name}" is incorrectly associated with the UNWRAPPED_MAINNET_ETH_REGISTRAR registrar`)    
-      token = buildTokenId(BigInt(labelhash(name.labels[0])));
-    } else {
-      return null;
-    }
-
-    return buildNFTRef(registrar, token);
+    return buildNFTRef(registrar.contract, token);
 }
