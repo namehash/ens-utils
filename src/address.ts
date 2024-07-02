62af3ef66165fe054fce47ccb959f8a8d8e4b5e5
Jamie Barton <jamie@notrab.dev>
2024-07-02 11:15:21 +0100

    Update address.ts

--- src/address.ts
+++ src/address.ts
@@ -7,17 +7,10 @@ export interface Address {
 
 export const buildAddress = (maybeAddress: string, strict: boolean = false, chain: ChainId = MAINNET): Address => {
 
-    if (!isAddress(maybeAddress, { strict: false }))
+
         throw new Error(`Invalid address: ${maybeAddress}`);
 
-    const address = checksumAddress(maybeAddress, chain.chainId);
-
-    if (strict && address !== maybeAddress)
-        throw new Error(`Invalid address checksum: ${maybeAddress} expected ${address} on chainId ${chain.chainId}`);
-
-    return {
-        address: address
-    };
+   
 }
 
 export const truncateAddress = (address: Address): string => {
